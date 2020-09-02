ce7da59db3e84e1796e7471685235ff571fea52b
ginowine <ginowine1@gmail.com>
2020-09-02 22:34:01 +0100

    fill method works

--- src/Machine.java
+++ src/Machine.java
@@ -28,8 +28,8 @@ public class Machine {
                 System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
                 buyCoffee(scanner.nextInt());
             } case "fill": {
-                
-            }
+                fillMachine();
+            }case
         }
 
     }
@@ -78,6 +78,26 @@ public class Machine {
                 break;
             }
         }
+    }
+
+    public static void fillMachine () {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water do you want to add: ");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk do you want to add: ");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans do you want to add: ");
+        int beans = scanner.nextInt();
+        System.out.println("Write how many disposable cups of coffee do you want to add: ");
+        int cups = scanner.nextInt();
+
+        totalAmtOfWater += water;
+        totalAmtOfMilk += milk;
+        totalAmtOfCoffeeBeans += beans;
+        totalAmtOfCups += cups;
+
+        remaining(totalAmtOfMoney, totalAmtOfWater, totalAmtOfMilk, totalAmtOfCoffeeBeans, totalAmtOfCups);
     }
 
 }
